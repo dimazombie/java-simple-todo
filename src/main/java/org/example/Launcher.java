29c521dcbf23d621c20f2b82b5e574d2d13d9927
Dimazombie <sushkevichd@gmail.com>
2020-08-26 21:16:19 -0700

    fix for windows

--- src/main/java/org/example/Launcher.java
+++ src/main/java/org/example/Launcher.java
@@ -6,9 +6,15 @@ import org.eclipse.jetty.webapp.WebAppContext;
 public class Launcher {
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
-        WebAppContext webapp = new WebAppContext("webapp", "/");
-        server.setHandler(webapp);
 
+        WebAppContext context = new WebAppContext("webapp", "/");
+
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+            // fix for Windows, so Jetty doesn't lock files
+            context.getInitParams().put("org.eclipse.jetty.servlet.Default.useFileMappedBuffer", "false");
+        }
+
+        server.setHandler(context);
         server.start();
     }
 }
