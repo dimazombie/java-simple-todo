ff53cec961003a962e72866dcd068f21a8ff8111
Dimazombie <sushkevichd@gmail.com>
2020-09-03 01:10:32 -0700

    added create and toggle operations

--- src/main/java/org/example/model/Task.java
+++ src/main/java/org/example/model/Task.java
@@ -1,11 +1,23 @@
 package org.example.model;
 
 public class Task {
+    int id;
     String title;
     Boolean isDone = false;
 
+    static int count = 0;
+
     public Task(String title) {
         this.title = title;
+        this.id = ++count;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -16,11 +28,11 @@ public class Task {
         this.title = title;
     }
 
-    public Boolean getDone() {
+    public Boolean getIsDone() {
         return isDone;
     }
 
-    public void setDone(Boolean done) {
+    public void setIsDone(Boolean done) {
         isDone = done;
     }
 }
--- src/main/java/org/example/rest/TaskResource.java
+++ src/main/java/org/example/rest/TaskResource.java
@@ -2,9 +2,8 @@ package org.example.rest;
 
 import org.example.model.Task;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -12,8 +11,25 @@ import static java.util.Arrays.asList;
 @Path("task")
 @Produces("application/json")
 public class TaskResource {
+
+    static List<Task> tasks = new ArrayList<>(
+            asList(new Task("task 1"), new Task("task 2")));
+
     @GET
     public List<Task> getAllTasks() {
-        return asList(new Task("task 1"), new Task("task 2"));
+        return tasks;
+    }
+
+    @POST
+    public Task createTask(@FormParam("title") String title) {
+        Task task = new Task(title);
+        tasks.add(task);
+        return task;
+    }
+
+    @PUT
+    public void toggle(@FormParam("id") int id) {
+        Task task = tasks.get(id-1);
+        task.setIsDone(!task.getIsDone());
     }
 }
